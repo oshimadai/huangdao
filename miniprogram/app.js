21d5cfa103c9d4b669c36cb4fae4b304c76d60e8
oshimadai <oshimadai@users.noreply.github.com>
2026-03-25 11:09:28 +0800

    test: 极简测试版排查黑屏

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,25 +1,12 @@
+// 极简测试版 app.js
 App({
   onLaunch: function () {
-    console.log('荒岛生存小程序启动')
-    this.initGameData()
+    console.log('App Launch!!!')
+  },
+  onShow: function () {
+    console.log('App Show!!!')
   },
   globalData: {
-    player: null
-  },
-  initGameData: function() {
-    const playerData = wx.getStorageSync('player')
-    if (playerData) {
-      this.globalData.player = playerData
-    } else {
-      this.globalData.player = {
-        name: '呆汪',
-        stage: 2,
-        hp: 100,
-        maxHp: 100,
-        gold: 500,
-        materials: {},
-        fightStyle: 'sustain'
-      }
-    }
+    test: 'hello'
   }
 })
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,39 +1,10 @@
-// 荒岛生存小程序 - 首页
-const app = getApp()
-
+// 极简测试页面
 Page({
-  data: {
-    testName: '荒岛测试',
-    loaded: false
-  },
-
   onLoad: function () {
-    console.log('===== 页面 onLoad =====')
-    console.log('app.globalData:', app.globalData)
-    this.setData({ loaded: true })
-    
-    // 测试提示
-    wx.showToast({
-      title: '页面加载成功',
-      icon: 'success',
-      duration: 2000
-    })
+    console.log('Page Load!!!')
   },
-
-  onReady: function () {
-    console.log('===== 页面 onReady =====')
-  },
-
-  onShow: function () {
-    console.log('===== 页面 onShow =====')
-  },
-
-  testTap: function () {
-    console.log('按钮被点击了！')
-    wx.showToast({
-      title: '点击成功！🎉',
-      icon: 'success',
-      duration: 1500
-    })
+  tap: function () {
+    console.log('Tap!!!')
+    wx.showToast({ title: '成功', icon: 'success' })
   }
 })
